1a415fb8bc069ae2a5001669758176d59337cba5
flckevin <quocanhbeo3010@gmail.com>
2023-04-05 03:52:45 +0700

    Added : - Post processing - tec9 - gun pistol (automatic) - M4A1 - gun assult rifle
    Changes:
    - lower graphic:
    + no shadow
    + material wont reflect

---
+++ Assets/Script/Weapons/Guns/GunMagBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/***************************************
+ * Authour: HAN
+ * Object hold:
+ * Content:
+ **************************************/
+public class GunMagBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
